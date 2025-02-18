21372afdb5530770be903e929d827ae56b04fa04
rprimsic <primsicr@gmail.com>
2025-02-18 16:19:11 +0300

    new branch for update testing

--- load_config.py
+++ load_config.py
@@ -1,12 +1,12 @@
 import json
 def load_config():
-    with open("data/config.json","r") as j:
+    with open("data\\config.json","r") as j:
         return json.load(j)
     
 def load_rewards():
-    with open("data/rewards.json","r") as j:
+    with open("data\\rewards.json","r") as j:
         return json.load(j)
 
 def save_config(config):
-    with open("data/config.json","w") as f:
+    with open("data\\config.json","w") as f:
         json.dump(config,f)
